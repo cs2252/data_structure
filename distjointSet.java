f7395a0ffb97190a03c93237d06faf43383fcee7
chandan kumar <cs2252@users.noreply.github.com>
2020-05-03 22:08:08 +0530

    add Disjoint set dataStructure file in java

---
+++ distjointSet.java
@@ -0,0 +1,42 @@
+class dsd{
+        public int[] p;
+        public int[] rank;
+
+        public dsd(int n)
+        {
+            p=new int[n];
+            rank=new int[n];
+            for(int i=0;i<n;i++)
+            {    p[i]=i;
+                rank[i]=i;
+            }   
+        }
+        int root(int a)
+        {
+            if(a==p[a])
+                return a;
+            
+            p[a]=root(p[a]);
+            return p[a];
+        }
+        void unite(int a,int b)
+        {
+            a=root(a);
+            b=root(b);
+            if(a==b)
+                return ;
+            if(rank[a]==rank[b])
+            {
+                rank[a]++;
+                p[b]=a;
+            }
+            else if(rank[a]>rank[b])
+            {
+                p[b]=a;   
+            }
+            else
+                p[a]=b;
+
+
+        }
+    }
